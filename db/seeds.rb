9e4df825abb2f853ccfb8b0208d4f441b2774778
Scott Macpherson <scottmacphersonmusic@gmail.com>
2016-03-21 00:47:42 -0700

    Style list index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 books = List.create(name: 'Books to Read')
 countries = List.create(name: 'Countries to Visit')
+shopping = List.create(name: 'Shopping')
+jobs = List.create(name: 'Places To Apply')
+terms = List.create(name: 'Terms To Study')
+names = List.create(name: 'Baby Names')
 
-def create_todo(list, body, priority)
-  list.todos.create!(body: body, priority: priority)
+def create_todo(list, body)
+  list.todos.create!(
+    body: body,
+    priority: Faker::Number.between(0, 5))
 end
 
-10.times do
-  create_todo(
-    books,
-    Faker::Book.title,
-    Faker::Number.between(0, 5))
-  create_todo(
-    countries,
-    Faker::Address.country,
-    Faker::Number.between(0, 5))
+15.times do
+  create_todo books,     Faker::Book.title
+  create_todo countries, Faker::Address.country
+  create_todo shopping,  Faker::Commerce.product_name
+  create_todo jobs,      Faker::Company.name
+  create_todo terms,     Faker::Hacker.ingverb
+  create_todo names,     Faker::Name.first_name
 end
